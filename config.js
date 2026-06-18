90f98aad925807b964a4510cc38057edcb032869
kawaguchidenki001 <kawaguchidenki001@gmail.com>
2026-06-18 21:44:49 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Gen-Can 共通設定 v106
+// GASを再デプロイしてURLが変わった場合は、この1ファイルだけ変更します。
+window.GENBA_CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyk8p6_gi6e3wdhQdWL0Oswz4BUtP3gR37PeFJJ9rO5mVhTRt4CikpQhK_bBwt1Ftr-/exec'
+};
