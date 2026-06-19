b7df76ca2c5bc169c299fb38b48a553cb1056f3d
kawaguchidenki001 <kawaguchidenki001@gmail.com>
2026-06-19 11:40:40 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// Gen-Can 共通設定 v110
+// Gen-Can 共通設定 v111
 // ファイル更新時は APP_VERSION / BUILD を必ず変更します。
-// v2026.06.19-10 工事写真台帳の現場選択表示修正
+// v2026.06.19-11 工事写真台帳をGoogleドライブ風の階層表示に簡素化
 // GASを再デプロイしてURLが変わった場合は GAS_URL だけ変更します。
 window.GENBA_CONFIG = {
   APP_NAME: 'Gen-Can',
-  APP_VERSION: 'v2026.06.19-10',
-  BUILD: '20260619-file-speed-09',
+  APP_VERSION: 'v2026.06.19-11',
+  BUILD: '20260619-ledger-simple-11',
   GAS_URL: 'https://script.google.com/macros/s/AKfycbyk8p6_gi6e3wdhQdWL0Oswz4BUtP3gR37PeFJJ9rO5mVhTRt4CikpQhK_bBwt1Ftr-/exec'
 };
