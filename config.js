26c94dd5b9aca2df9a0cbaf58bff75167afbec2b
kawaguchidenki001 <kawaguchidenki001@gmail.com>
2026-06-19 10:36:31 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-// Gen-Can 共通設定 v109
+// Gen-Can 共通設定 v110
 // ファイル更新時は APP_VERSION / BUILD を必ず変更します。
+// v2026.06.19-09 ファイル階層表示の高速化キャッシュ対応
 // GASを再デプロイしてURLが変わった場合は GAS_URL だけ変更します。
 window.GENBA_CONFIG = {
   APP_NAME: 'Gen-Can',
-  APP_VERSION: 'v2026.06.19-08',
-  BUILD: '20260619-drive-hierarchy-08',
+  APP_VERSION: 'v2026.06.19-09',
+  BUILD: '20260619-file-speed-09',
   GAS_URL: 'https://script.google.com/macros/s/AKfycbyk8p6_gi6e3wdhQdWL0Oswz4BUtP3gR37PeFJJ9rO5mVhTRt4CikpQhK_bBwt1Ftr-/exec'
 };
